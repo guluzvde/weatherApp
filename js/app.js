a61dd4e7866f5bb5ebfbd2e354072eaaa3340a89
Ilkin Guluzada <glzdreal@gmail.com>
2022-07-15 02:06:29 +0400

    update

--- js/app.js
+++ js/app.js
@@ -5,10 +5,11 @@ const temp = document.querySelector(".weather");
 const wind = document.querySelector(".wind");
 const rutubet = document.querySelector(".hum");
 
+
+
 let dayNow = new Date().getDate();
 let monthNow = new Date().getMonth();
 let yearNow = new Date().getFullYear();
-
 const aylar = {
   0: "Jan",
   1: "Feb",
@@ -27,31 +28,42 @@ let dateNow = document.createElement("h3");
 dateNow.innerHTML = `${dayNow}  ${aylar[monthNow]}  ${yearNow}`;
 date.appendChild(dateNow);
 
+document.onload=myLocation();
 function myLocation() {
   fetch("https://ipapi.co/json/")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      let h1 = document.createElement("h1");
-      h1.innerHTML = `${data.city}`;
-      h1.style.color = "gold";
-      city.appendChild(h1);
-    });
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let h1 = document.createElement("h1");
+    h1.innerHTML = `${data.city}`;
+    h1.style.color = "gold";
+    h1.className="sheher"
+    city.appendChild(h1);
+  });
 }
-myLocation();
 
-xhr.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    // console.log( JSON.parse(xhr.responseText));
-    temp.innerHTML = `${
-      JSON.parse(xhr.responseText).current.feelslike_c
-    }  ${"\u2103"}`;
-    wind.innerHTML = `${JSON.parse(xhr.responseText).current.wind_mph} mph`;
-    rutubet.innerHTML = `${JSON.parse(xhr.responseText).current.humidity}%`;
-  }
-};
+setTimeout(() => {
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // console.log( JSON.parse(xhr.responseText));
+      temp.innerHTML = `${
+        JSON.parse(xhr.responseText).current.feelslike_c
+      }  ${"\u2103"}`;
+      wind.innerHTML = `${JSON.parse(xhr.responseText).current.wind_mph} mph`;
+      rutubet.innerHTML = `${JSON.parse(xhr.responseText).current.humidity}%`;
+    }
+  };
+
 url =
-  "https://api.weatherapi.com/v1/current.json?key=a0e8927c61a1441a8d8183910221407&q=baku&aqi=no";
+`https://api.weatherapi.com/v1/current.json?key=a0e8927c61a1441a8d8183910221407&q=${city.textContent}&aqi=no`;
 xhr.open("GET", url);
 xhr.send();
+
+}, 500);
+
+
+// 
+// setTimeout(() => {
+//   console.log(city.textContent);
+// }, 1000);
