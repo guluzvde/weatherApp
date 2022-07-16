9b66a2207824dff3aef39a27038e92d919a9e7c0
Ilkin Guluzada <glzdreal@gmail.com>
2022-07-16 04:56:18 +0400

    bugs_fixed

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-const xhr = new XMLHttpRequest();
+//############################  Seçmə/Təyin etmə START  ############################
 const city = document.querySelector(".city");
 const date = document.querySelector(".date");
 const temp = document.querySelector(".weather");
 const wind = document.querySelector(".wind");
 const rutubet = document.querySelector(".hum");
-
-
-
 let dayNow = new Date().getDate();
 let monthNow = new Date().getMonth();
 let yearNow = new Date().getFullYear();
@@ -24,40 +21,44 @@ const aylar = {
   10: "Nov",
   11: "Dec",
 };
+//############################  Seçmə/Təyin etmə END  ############################
+positionWeather()
+//############################  Koordinatların təyini və APİ-dən koor-a uyğun məlumat alınması START  ############################
+ function positionWeather (pos) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    let lat = position.coords.latitude
+    let lon= position.coords.longitude
+//############################  APİ-lərə müraciət START  ############################
+const xhr = new XMLHttpRequest();
+     xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          let h1 = document.createElement("h1");
+              h1.innerHTML = JSON.parse(xhr.responseText).location.region;
+              h1.style.color = "gold";
+              h1.className="sheher"
+              city.appendChild(h1);
+
+          temp.innerHTML = `${
+            JSON.parse(xhr.responseText).current.feelslike_c
+          }  ${"\u2103"}`;
+          wind.innerHTML = `${JSON.parse(xhr.responseText).current.wind_mph} mph`;
+          rutubet.innerHTML = `${JSON.parse(xhr.responseText).current.humidity}%`;
+        }
+      };
+      url =
+      `https://api.weatherapi.com/v1/current.json?key=a0e8927c61a1441a8d8183910221407&q=${lat},${lon}&aqi=no
+      `;
+      xhr.open("GET", url);
+      xhr.send();
+//#############################  APİ-lərə müraciət END  #############################
+  },
+  function (error) {
+      console.log("The Locator was denied. :(")
+  })
+};
+//############################  Koordinatların təyini və APİ-dən koor-a uyğun məlumat alınması END  ############################
+//############################  Hal-hazırdakı tarix START  ############################
 let dateNow = document.createElement("h3");
 dateNow.innerHTML = `${dayNow}  ${aylar[monthNow]}  ${yearNow}`;
 date.appendChild(dateNow);
-
-document.onload=myLocation();
-function myLocation() {
-  fetch("https://ipapi.co/json/")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    let h1 = document.createElement("h1");
-    h1.innerHTML = `${data.city}`;
-    h1.style.color = "gold";
-    h1.className="sheher"
-    city.appendChild(h1);
-  });
-}
-
-setTimeout(() => {
-  xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      // console.log( JSON.parse(xhr.responseText));
-      temp.innerHTML = `${
-        JSON.parse(xhr.responseText).current.feelslike_c
-      }  ${"\u2103"}`;
-      wind.innerHTML = `${JSON.parse(xhr.responseText).current.wind_mph} mph`;
-      rutubet.innerHTML = `${JSON.parse(xhr.responseText).current.humidity}%`;
-    }
-  };
-
-url =
-`https://api.weatherapi.com/v1/current.json?key=a0e8927c61a1441a8d8183910221407&q=${city.textContent}&aqi=no`;
-xhr.open("GET", url);
-xhr.send();
-
-}, 500);
+//############################  Hal-hazırdakı tarix END  ############################
